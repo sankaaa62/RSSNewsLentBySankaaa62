261fbdc9961d80ead6909b4596893956f332e529
sankaaa62 <kandakov.sasha@gmail.com>
2019-05-24 01:45:39 +0500

    Оптимизован ListView с помощью статического класса ViewHolder содержащего ссылки на ячейки

--- app/src/main/java/com/example/rssnewslentbysankaaa62/PostItemAdapter.java
+++ app/src/main/java/com/example/rssnewslentbysankaaa62/PostItemAdapter.java
@@ -22,22 +22,32 @@ public class PostItemAdapter extends ArrayAdapter<PostData> {
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = myContext.getLayoutInflater();
-        View rowView = inflater.inflate(R.layout.postitem, null);
-        ImageView thumbImageView = (ImageView) rowView
-                .findViewById(R.id.postThumb);
-        if (datas[position].postThumbUrl == null) {
-            thumbImageView.setImageResource(R.drawable.postthumb_loading);
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = myContext.getLayoutInflater();
+            convertView = inflater.inflate(R.layout.postitem, null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.postThumbView = (ImageView) convertView
+                    .findViewById(R.id.postThumb);
+            viewHolder.postTitleView = (TextView) convertView
+                    .findViewById(R.id.postTitleLabel);
+            viewHolder.postDateView = (TextView) convertView
+                    .findViewById(R.id.postDateLabel);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView postTitleView = (TextView) rowView
-                .findViewById(R.id.postTitleLabel);
-        postTitleView.setText(datas[position].postTitle);
+        if (datas[position].postThumbUrl == null) {
+            viewHolder.postThumbView
+                    .setImageResource(R.drawable.postthumb_loading);
+        }
 
-        TextView postDateView = (TextView) rowView
-                .findViewById(R.id.postDateLabel);
-        postDateView.setText(datas[position].postDate);
+        viewHolder.postTitleView.setText(datas[position].postTitle);
+        viewHolder.postDateView.setText(datas[position].postDate);
 
-        return rowView;
+        return convertView;
     }
 }
---
+++ app/src/main/java/com/example/rssnewslentbysankaaa62/ViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.rssnewslentbysankaaa62;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewHolder {
+    TextView postTitleView;
+    TextView postDateView;
+    ImageView postThumbView;
+}
